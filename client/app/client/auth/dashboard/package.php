bafb5ec47c0fc95cc40420e102f77a58a987b7b4
keshridivya <dkeshari094@gmail.com>
2022-12-13 17:33:33 +0530

    link

--- client/app/client/auth/dashboard/package.php
+++ client/app/client/auth/dashboard/package.php
@@ -57,7 +57,7 @@ $packageId=$_GET['packageId'];
 <!-- BEGIN: Head-->
 
 <head>
-    <base href="http://localhost:8000/tectignis_crm/client/app/client/auth/dashboard/">
+    <base href="https://leads.tectignis.in/client/app/client/auth/dashboard/">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
